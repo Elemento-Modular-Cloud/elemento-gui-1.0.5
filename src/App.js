276a44374f03a0cfe2c73b5121727aa256acc56d
Francesco Chpl <francesco@chpl.it>
2023-03-06 17:45:14 +0100

    feat(react): react boilerplate, clean up code, standard js linting

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        Elemento
+      </div>
+    )
+  }
+}
+
+export default App
