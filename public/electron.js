601806b3c92b6bd1c76df087614bd6773b9fdaec
Francesco Chpl <francesco@chpl.it>
2023-03-31 18:01:57 +0200

    feat(electron): added functionality to spawn third party processes from electron app

--- public/electron.js
+++ public/electron.js
@@ -1,9 +1,9 @@
 const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
-
 const path = require('path')
 const isDev = require('electron-is-dev')
+const { spawn } = require('child_process')
 
 let mainWindow
 
@@ -13,6 +13,46 @@ function createWindow () {
   mainWindow.on('closed', () => { mainWindow = null })
   mainWindow.setSize(800, 600, true)
   // mainWindow.setKiosk(true)
+
+  // Start the Node.js API
+  try {
+    const apiPath = path.join(__dirname, 'api', 'server.js')
+    spawn('node', [apiPath], { stdio: 'inherit' })
+  } catch (error) {
+    window.alert(error)
+  }
+
+  // Spawn a new Python process and execute the script
+  try {
+    const pythonProcess = spawn('python3', [path.join(__dirname, 'python', 'script.py')])
+    pythonProcess.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`)
+    })
+    pythonProcess.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`)
+    })
+    pythonProcess.on('close', (code) => {
+      console.log(`child process exited with code ${code}`)
+    })
+  } catch (error) {
+    window.alert(error)
+  }
+
+  // Spawn a new process and execute docker-compose up
+  try {
+    const dockerComposeProcess = spawn('docker-compose', ['-f', path.join(__dirname, 'docker', 'docker-compose.yml'), 'up', '-d'])
+    dockerComposeProcess.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`)
+    })
+    dockerComposeProcess.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`)
+    })
+    dockerComposeProcess.on('close', (code) => {
+      console.log(`child process exited with code ${code}`)
+    })
+  } catch (error) {
+    window.alert(error)
+  }
 }
 
 app.on('ready', createWindow)
