5b78f7a23c19742d84f305d0d9c141690ef26269
Francesco Chpl <francesco@chpl.it>
2023-05-01 13:29:44 +0200

    chore(utils): added compact string function to render vm compat name in adv mode

--- src/Global/Utils.js
+++ src/Global/Utils.js
@@ -1,3 +1,5 @@
+import md5 from 'md5'
+
 function formatBytes (bytes, decimals = 0) {
   if (!+bytes) return '0 Bytes'
 
@@ -10,8 +12,33 @@ function formatBytes (bytes, decimals = 0) {
   return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
 }
 
+function toRGB (s) {
+  let hash = 0
+  if (s.length === 0) return hash
+  for (let i = 0; i < s.length; i++) {
+    hash = s.charCodeAt(i) + ((hash << 5) - hash)
+    hash = hash & hash
+  }
+  const rgb = [0, 0, 0]
+  for (let j = 0; j < 3; j++) {
+    rgb[j] = (hash >> (j * 8)) & 255
+  }
+  return `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
+}
+
+function random (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function compactString (s, size) {
+  const hash = md5(s || 'Elemento')
+  return `${hash.charAt(random(0, hash.length))}${hash.charAt(random(0, hash.length))}${hash.charAt(random(0, hash.length))}${hash.charAt(random(0, hash.length))}`
+}
+
 const Utils = {
-  formatBytes
+  formatBytes,
+  toRGB,
+  compactString
 }
 
 export default Utils
