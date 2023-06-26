56e8a81695e2a01b355d80bb339fffdd49c2488f
Francesco Chpl <francesco@chpl.it>
2023-06-26 14:24:18 +0200

    chore(api): increment api timeout value to 10 mins

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -11,7 +11,7 @@ let client = null
 Api.createClient = (baseURL) => {
   client = apisauce.create({
     baseURL,
-    timeout: 20000,
+    timeout: 600000,
     headers: {
       'Content-Type': 'application/json',
       Authorization: Api.jwt
