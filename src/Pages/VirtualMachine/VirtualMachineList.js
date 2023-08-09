e7a1d9138609a641355082ca04fd58720d407686
Francesco Chpl <francesco@chpl.it>
2023-08-09 11:29:05 +0200

    chore(network): remove double quotes

--- src/Pages/VirtualMachine/VirtualMachineList.js
+++ src/Pages/VirtualMachine/VirtualMachineList.js
@@ -130,9 +130,9 @@ class VirtualMachineList extends Component {
                         <td>{detail.os_family}</td>
                         <td>{detail.os_flavour}</td>
                         <td>{detail.creation_date}</td>
-                        <td>{JSON.stringify(detail.network_config?.ipv4)}</td>
+                        <td>{detail.network_config?.ipv4}</td>
                         {/* <td><button className='bn632-hover bn22' onClick={async () => !toBeDeleted && window.open(detail.viewer, '_blank')}>Viewer</button></td> */}
-                        <td>ssh {JSON.stringify(detail.network_config?.ipv4)}</td>
+                        <td>ssh {detail.network_config?.ipv4}</td>
                         <td><button className='bn632-hover bn28' onClick={async () => !toBeDeleted && await this.deleteVirtualMachine(uniqueID)}>Delete</button></td>
                       </tr>
                     )
