220f0410a27b25909a7efd989957b7327c8adfe8
Francesco Chpl <francesco@chpl.it>
2023-08-11 17:23:40 +0200

    chore(sidebar): return back to home after click on logo

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -34,7 +34,7 @@ const Sidebar = ({ selected }) => {
 
   return (
     <div className='sidebar'>
-      <div className='sidelogo'>
+      <div className='sidelogo' onClick={() => navigate('/')}>
         <LogoInlineWhite />
       </div>
 
