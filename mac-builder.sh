3949544b5a948096aaa7d0112d6d91f6e0e29723
filippo-ferrando <fferrando@elemento.cloud>
2024-08-14 11:24:06 +0200

    test exports

---
+++ mac-builder.sh
@@ -0,0 +1,6 @@
+yarn install
+yarn install --check-files
+export APPLE_ID=fferrando@elemento.cloud
+export APPLE_APP_SPECIFIC_PASSWORD=iswt-hmsx-xinw-ppef
+yarn build
+yarn electron:dist-mac-arm
--- notarize.js
+++ notarize.js
@@ -5,7 +5,7 @@ async function notarizeApp () {
   console.log('Try to notarize the app...')
   await notarize({
     appBundleId: 'app.elemento.cloud',
-    appPath: path.resolve(__dirname, 'distr/mac-arm64/ElectrOS.app'),
+    appPath: path.resolve(__dirname, 'dist/mac-arm64/ElectrOS.app'),
     appleId: 'fferrando@elemento.cloud',
     appleIdPassword: 'iswt-hmsx-xinw-ppef',
     teamId: '9WTDB7G2C7',
