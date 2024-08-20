c2c976d930507b255ad65eb6169a41b1c787ef76
filippo-ferrando <fferrando@elemento.cloud>
2024-08-20 11:28:19 +0200

    build scripts for macOS

---
+++ mac-scripts/x86-builder.sh
@@ -0,0 +1,7 @@
+yarn clean:build
+yarn install
+yarn install --check-files
+export APPLE_ID=fferrando@elemento.cloud
+export APPLE_APP_SPECIFIC_PASSWORD=iswt-hmsx-xinw-ppef
+yarn build
+yarn electron:dist-mac-x64
